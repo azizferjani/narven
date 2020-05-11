45ea29469afde73a5d0ab6f652a1fbb03393956f
Aziz ferjani <aziz300ferjani@gmail.com>
2020-05-11 10:06:48 +0100

    Updates

---
+++ precache-manifest.727ba74f8101a1e3562d425863904428.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f44d0fd998ecd54789a6dc5a2a32f869",
+    "url": "/index.html"
+  },
+  {
+    "revision": "006218c9b0abb6ce3455",
+    "url": "/static/css/main.c66d4e0f.chunk.css"
+  },
+  {
+    "revision": "8ac721244f003fbccdd9",
+    "url": "/static/js/2.02db51bb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.02db51bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "006218c9b0abb6ce3455",
+    "url": "/static/js/main.71da6a42.chunk.js"
+  },
+  {
+    "revision": "d6bf62209845d87d1f40",
+    "url": "/static/js/runtime-main.b6ca98df.js"
+  }
+]);
